22c038b9e688d7a9c76329f48d2439af789223d6
Igor Osadchyi <igor@webio.net.ua>
2020-10-18 12:39:58 +0300

    feat: implement check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+    let prevStr;
+
+    while (str !== prevStr) {
+        prevStr = str;
+        bracketsConfig.map(([open, close]) => {
+            str = str.replace(`${open}${close}`, '');
+        });
+    }
+
+    return !str;
 }
+
+module.exports = check;
